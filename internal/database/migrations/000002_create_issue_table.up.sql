49f304997d66a67191c7a6243a988cf1aed630a6
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2025-10-26 00:05:46 +0200

    create issue table migrations

---
+++ internal/database/migrations/000002_create_issue_table.up.sql
@@ -0,0 +1,9 @@
+create table if not exists issue (
+    id uuid primary key,
+    title text not null,
+    description text not null,
+    image_url text,
+    link_id uuid references link(id) on delete set null,
+    created_at timestamp with time zone default current_timestamp,
+    updated_at timestamp with time zone default current_timestamp
+);
