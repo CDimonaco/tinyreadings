d8679d4f01ff7c886d573a7a7789c943bc905952
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2025-10-26 00:48:28 +0200

    Add configuration struct with validation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cdimonaco/tinyreadings
 
 go 1.25.3
+
+require github.com/caarlos0/env/v11 v11.3.1
---
+++ internal/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import "github.com/caarlos0/env/v11"
+
+type Config struct {
+	DBHost     string `env:"DB_HOST,required"`
+	DBUser     string `env:"DB_USER,required"`
+	DBPassword string `env:"DB_PASSWORD,required"`
+	DBPort     int    `env:"DB_PORT,required"`
+	DBName     string `env:"DB_NAME,required"`
+	Env        string `env:"ENV" envDefault:"development"`
+}
+
+func LoadConfig() (*Config, error) {
+	config := Config{}
+	err := env.Parse(&config)
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
