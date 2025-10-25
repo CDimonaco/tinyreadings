d6e8da4e1bb226f850ecd393ee32d13abfd6e4fa
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2025-10-26 00:55:23 +0200

    wire config with dotenv

--- cmd/tinyreadings/main.go
+++ cmd/tinyreadings/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 
+	"github.com/cdimonaco/tinyreadings/internal/config"
+	"github.com/cdimonaco/tinyreadings/internal/logger"
 	"github.com/cdimonaco/tinyreadings/internal/version"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Printf(
-		"Hello, TinyReadings!, Version: %s, Build Date: %s\n",
-		version.Version,
-		version.BuildDate,
-	)
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	config, err := config.LoadConfig()
+	if err != nil {
+		panic(err)
+	}
+	logger.ConfigureGlobalLogger(config)
+
+	slog.Info("TinyReadings starting up", "version", version.Version, "build", version.BuildDate)
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/cdimonaco/tinyreadings
 go 1.25.3
 
 require github.com/caarlos0/env/v11 v11.3.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
